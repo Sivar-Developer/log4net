385236e7279359287e19f08d6d87de2a8ca8e144
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-12-04 00:02:30 +0300

    Configure log4net for console app

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+[assembly: log4net.Config.XmlConfigurator(Watch=true)]
+
 namespace ConsoleUI
 {
     class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
